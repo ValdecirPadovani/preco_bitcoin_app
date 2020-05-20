6aa99973eb544d9da979aa9ad1fb429ca1cdd857
valdecir.junior <valdecirpadovani.jr@gmail.com>
2020-05-20 16:37:19 -0300

    Preco BitCoin para estudo de web service

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:preco_bitcoin_app/Home.dart';
+
+void main() => runApp(
+    MaterialApp(
+    home: Home(),
+      debugShowCheckedModeBanner: false,
+));
